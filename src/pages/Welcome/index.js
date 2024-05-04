f24ea5effc74cfdfc99dde4e3670b03cc15727b7
Yann Lima <yann_athayde@hotmail.com>
2024-05-04 14:54:13 -0300

    Alteração no App.js, novas dependencias (pagina de txt com os comandos colocado) + LoginPage

---
+++ src/pages/Welcome/index.js
@@ -0,0 +1,20 @@
+import { 
+    View, 
+    Text,
+    StyleSheet
+ } from 'react-native'
+import React from 'react'
+
+export default function Welcome() {
+  return (
+    <View style={style.container}>
+      
+    </View>
+  )
+}
+
+const style = StyleSheet.create({
+    container:{
+
+    }
+})
