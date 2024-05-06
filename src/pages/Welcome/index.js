7601709feb980471e47c664409edbcdfc1709973
Yann Lima <yann_athayde@hotmail.com>
2024-05-06 09:38:29 -0300

    Conclusão da pagina welcome, vinculação do sign email a sua respectiva pagina

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -7,9 +7,16 @@ import {
     TouchableOpacity
  } from 'react-native';
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 /*import LinearGradient from 'react-native-linear-gradient';*/
 
 export default function Welcome() {
+  const navigation = useNavigation();
+
+  const handleEmailSignIn = () =>{
+    navigation.navigate('SignIn');
+  };
+  
   return (
     <ImageBackground
     source={require('../../../assets/background_home.png')}
@@ -23,17 +30,62 @@ export default function Welcome() {
       <View style={style.containerLogo}>
         <Image
         source={require('../../../assets/logo.png')}
-        style={{width: '100%'}}
+        style={{width: '40%'}}
         resizeMode='contain'
         />
       </View>
 
       <View style={style.containerForm}>
         <Text style={style.title}>Challenge Sport Club</Text>
-        <Text style={style.text}>User</Text>
+        {/*Espaço para imagem do perfil */}
+        <View style={style.profileImageContainer}>
+          <Image
+            source={require('../../../assets/man_profile.jpg')}
+            style={style.profileImage}
+            resizeMode='cover'
+          />
+        </View>
+        <Text style={style.title}>Hello, user</Text>
 
-        <TouchableOpacity style={style.button}>
-          <Text style={style.buttonText}>Login</Text>
+        {/*Botões de Sign in*/}
+        <View style={style.buttonContainer}>
+          <TouchableOpacity style={style.signInButton}>
+          <View style={style.buttonContent}>
+              <Image 
+                source={require('../../../assets/logoFacebook.png')}
+                style={style.buttonImage}
+                resizeMode='contain'
+              />
+              <Text style={style.buttonText}>Sign in with Facebook</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={style.signInButton}>
+          <View style={style.buttonContent}>
+            <Image 
+                source={require('../../../assets/logoApple.png')}
+                style={style.buttonImage}
+                resizeMode='contain'
+              />
+              <Text style={style.buttonText}>Sign in with Apple</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={style.signInButton} onPress={handleEmailSignIn}>
+          <View style={style.buttonContent}>
+            <Image 
+                source={require('../../../assets/logoEmail.png')}
+                style={style.buttonImage}
+                resizeMode='contain'
+              />
+              <Text style={style.buttonText}>Sign in with Email</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        {/*Se não tiver conta - criar conta*/}
+        <TouchableOpacity style={style.createButton}>
+          <Text style={style.createText}>Create account</Text>
         </TouchableOpacity>
       </View>
 
@@ -51,17 +103,72 @@ const style = StyleSheet.create({
   },
   overlay: {
     ...StyleSheet.absoluteFillObject,
-    backgroundColor: 'rgba(120, 203, 229, 0.65)', // Cor com 65% de opacidade
+    backgroundColor: 'rgba(120, 203, 229, 0.65)',
   },
     container:{
       flex: 1,
-      //backgroundColor: '#38a69d',
     },
     containerLogo:{
-      flex:2,
-     // backgroundColor: 'red'
+      justifyContent: 'flex-start',
+      alignItems:'center',
+      marginTop: '10%', //distacia da parte superior da tela
+     //
     },
-    containerFrom:{
-      flex:1,
+    containerForm:{
+      //flex:1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    title:{
+      fontSize: 28,
+      color: '#f5f5f5',
+      fontFamily:'Arial', //alterar fonte
+      textAlign: 'center',
+    },
+    //Imagem de perfil do usuario
+    profileImageContainer:{
+      margin: 30, //espaço entre o titulo e a imagem de perfil
+      width: 120,
+      height:120,
+      borderRadius: 60,//imagem redonda
+      overflow: 'hidden', //para cortar a imagem de forma redonda
+      borderWidth: 2,
+      borderColor: '#29D9D5',
+    },
+    profileImage: {
+      width: '100%',
+      height: '100%',
+      borderRadius: 50,
+    },
+    //Estilos para os botoes de sigin in
+    buttonContainer: {
+      marginTop: 30,
+      alignItems: 'center',
+    },
+    signInButton:{
+      width: 260,
+      height: 50,
+      backgroundColor: 'white',
+      borderRadius: 15,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 10,
+    },
+    buttonContent:{
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    buttonImage:{
+      marginRight: 10,
+    },
+    buttonText:{
+      color: 'black',
+      fontSize: 16,
+    },
+    //Criar conta
+    createText:{
+      fontSize: 16,
+      color: '#fff',
+      marginTop: 40,
     }
 })
